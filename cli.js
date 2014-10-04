196f55c4b4aa928e640d30fc0432ae4c313ca0f1
Michał Budzyński <michal@virtualdesign.pl>
2014-10-04 01:01:00 +0100

    [PROVERBIUM] - :snowman: empty space

--- cli.js
+++ cli.js
@@ -3,7 +3,8 @@
 var proverbium = require('./');
 var clc = require('cli-color');
 
-proverbium(function(result){
+proverbium(function(result) {
+  console.log('');
   console.log('1st proverb: ', clc.white(result.first));
   console.log('2nd proverb: ', clc.white(result.second));
   console.log('Mix: ', clc.redBright.bold(result.mix));
