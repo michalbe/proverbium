150da6e6f95f3f3c0a10fc31325e56eedb6bfd74
Michał Budzyński <michal@virtualdesign.pl>
2014-10-04 01:01:00 +0100

    [PROVERBIUM] - :tiger: moar labels

--- cli.js
+++ cli.js
@@ -6,5 +6,5 @@ var clc = require('cli-color');
 proverbium(function(result){
   console.log('1st proverb: ', clc.white(result.first));
   console.log('2nd proverb: ', clc.white(result.second));
-  console.log(clc.redBright.bold(result.mix));
+  console.log('Mix: ', clc.redBright.bold(result.mix));
 });
