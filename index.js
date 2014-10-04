fccb9fa06d7c6434b89d35b8e437f8cc54e1768c
Michał Budzyński <michal@virtualdesign.pl>
2014-10-04 01:01:00 +0100

    [PROVERBIUM] - :house_with_garden: random modifiers

--- index.js
+++ index.js
@@ -3,25 +3,25 @@
 var rpp = require('random-polish-proverb');
 
 var getFirst = function(first) {
-  if (~first.indexOf(',')) {
+  if (~first.indexOf(',') && ~~(Math.random()*3) > 0) {
     return first.split(',')[0];
   } else if (~first.indexOf(';')) {
     return first.split(';')[0];
   } else {
     var splitted = first.split(' ');
-    var wordsCount = Math.round(splitted.length/2);
+    var wordsCount = Math.round(splitted.length/2) + (1-~~(Math.random()*3));
     var words = splitted.slice(0, wordsCount);
     return words.join(' ');
   }
 };
 var getLast = function(last) {
-  if (~last.indexOf(',')) {
+  if (~last.indexOf(',') && ~~(Math.random()*3) > 0) {
     return last.split(',').pop();
   } else if (~last.indexOf(';')) {
     return last.split(';').pop();
   } else {
     var splitted = last.split(' ');
-    var wordsCount = Math.round(splitted.length/2);
+    var wordsCount = Math.round(splitted.length/2) + (1-~~(Math.random()*3));
     var words = splitted.slice(Math.max(splitted.length - wordsCount, 1));
     return words.join(' ');
   }
