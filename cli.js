be0527274d7fa54d9f47f1c4d54088a45de31396
Michał Budzyński <michal@virtualdesign.pl>
2014-10-04 01:01:00 +0100

    [PROVERBIUM] - :bell: bold in result

--- cli.js
+++ cli.js
@@ -6,5 +6,5 @@ var clc = require('cli-color');
 proverbium(function(result){
   console.log(clc.white(result.first));
   console.log(clc.white(result.second));
-  console.log(clc.redBright(result.mix));
+  console.log(clc.redBright.bold(result.mix));
 });
